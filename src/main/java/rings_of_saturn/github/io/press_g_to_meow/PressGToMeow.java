adc43f89be570eccc4ea9a1342b707e11c6ea6b8
TLS <thecolorizedsystem@gmail.com>
2024-11-25 00:15:22 +0200

    ⭐Initial Commit!

--- src/main/java/rings_of_saturn/github/io/press_g_to_meow/PressGToMeow.java
+++ src/main/java/rings_of_saturn/github/io/press_g_to_meow/PressGToMeow.java
@@ -3,12 +3,10 @@ package rings_of_saturn.github.io.press_g_to_meow;
 import net.fabricmc.api.ModInitializer;
 
 public class PressGToMeow implements ModInitializer {
-    public static String MOD_ID = "press_g_to_meow";
 
 
     @Override
     public void onInitialize() {
-        //Sounds.registerSounds();
 
     }
 }
